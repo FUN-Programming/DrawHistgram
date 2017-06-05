289cfc2afd7b083ee8786e52fb98259e99b81347
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-06-05 09:14:31 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+import processing.core.PApplet
+
+class Main : PApplet() {
+
+    override fun settings() {
+        super.settings()
+        size(500, 500)
+    }
+
+    override fun setup() {
+        super.setup()
+    }
+}
+
+fun main(args: Array<String>) {
+    PApplet.main("Main")
+}
